5f0224a08aa3153aa916c4295acb0fa477b103d0
vcoronado2014 <vcoronado.alarcon@gmaiil.com>
2021-10-28 20:32:04 -0300

    primer commit

---
+++ Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext
+    {
+        public SalvoContext(DbContextOptions<SalvoContext> options):base(options)
+        {
+        }
+    }
+}
